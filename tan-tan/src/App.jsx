24b0ba28224c488604799d7f6b8a319dfb0c318f
Misteaken-Tan <tristanliwanag9@gmail.com>
2025-08-26 22:15:45 +0800

    First-Commit

---
+++ tan-tan/src/App.jsx
@@ -0,0 +1,33 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div id="fab">
+        <i class="fa-regular fa-headphones"></i>
+
+      </div>
+
+      <div id="music-container">
+        <div className="music-card">
+
+          <img src="https://imgs.search.brave.com/7mmO6bIBYFiYLtTV2BUfWJYpCosqPjyFEsMhtNZ08p4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9iMzcy/MDc4Ny5zbXVzaGNk/bi5jb20vMzcyMDc4/Ny93cC1jb250ZW50/L3VwbG9hZHMvMjAy/NS8wNS9DVVAtT0Yt/Sk9FLUJBTk5FUi53/ZWJwP2xvc3N5PTIm/c3RyaXA9MSZ3ZWJw/PTE" alt="" />
+          <h3>Multu</h3>
+          
+          <p>VIEWS 1,000,000</p>
+          <i class="fa-regular fa-headphones play-icon"></i>
+
+        </div>
+        
+
+        
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
